42183f0a010866aad1aca0b128776c3fe1f80431
Zou Zhefeng <archer.zou84@gmail.com>
2025-06-13 23:37:50 +1200

    finish the landing page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { provideRedux } from '@reduxjs/angular-redux';
+import { store } from './store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideRedux({ store }),
+    provideApollo(() => {
+      const httpLink: HttpLink = inject(HttpLink);
+      const apiUrl = environment.apiUrl;
+      return {
+        link: httpLink.create({uri: apiUrl, withCredentials: true}),
+        cache: new InMemoryCache({
+          addTypename: false
+        })
+      }
+    }, {
+      useMutationLoading: true
+    }),
+]
 };
